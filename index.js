7167e7e043c6d81b6d37a1d2b5d8bffebe931d52
sbtech24 <bajomosemilore@gmail.com>
2023-09-10 10:58:12 +0100

    last

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const app = express();
+const moment = require('moment')
 
 const Day = new Date()
 const days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
 const currentDay = days[Day.getDay()]
-
-const date  = new Date().toISOString().slice(0, -5) + "Z"
-console.log(date)
+const date = moment();
+const utcTime  = date.toISOString().slice(0, -5) + "Z"
+console.log(utcTime)
 
 app.get('/api',(req,res)=>{
 let track = req.query.track
@@ -16,7 +17,7 @@ let slack_name = req.query.slack_name
 res.status(200).json({
     "slack_name":slack_name,
     "current_day":currentDay,
-    "utc_time":date,
+    "utc_time":utcTime,
     "track":track,
     "github_file_url":"https://github.com/Sbtech24/Hng-first-task/blob/main/index.js",
     "github_repo_url":"https://github.com/Sbtech24/Hng-first-task/tree/main",
@@ -28,5 +29,4 @@ res.status(200).json({
 app.listen(5001,()=>{
     console.log('The server is listeninig on port 5001')
 })
-
 
